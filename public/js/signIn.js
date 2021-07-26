a2f4b33fbfa516580adaf070ca38ea952a8d7ac8
ethan-kwon <ethan200304@gmail.com>
2021-07-26 17:43:11 +0000

    redirect working

--- public/js/signIn.js
+++ public/js/signIn.js
@@ -1,4 +1,11 @@
 const signInButton = document.querySelector(".button");
 signInButton.addEventListener("click", () => {
-    
+    const provider = new firebase.auth.GoogleAuthProvider()
+    firebase.auth().signInWithPopup(provider).then(result => {
+        const user = result.user;
+        console.log("login success", user);
+    })
+    .catch(error => {
+        console.elog("Login failed", error)
+    })
 })
